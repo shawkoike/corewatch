06cce513dd0435f9acfa9799325ea2c12a10eba1
小池翔 <shaw@koikeshounoMacBook-Pro.local>
2019-10-02 00:03:53 +0900

    プロジェクト新規作成

--- corewatchapp/app/controllers/corewatch_controller.rb
+++
@@ -1,5 +0,0 @@
-# 未ログイン時TOP 画面用コントローラー
-class CorewatchController < ApplicationController
-  def index
-  end
-end
--- corewatchapp/config/routes.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.routes.draw do
-  devise_for :users
-  root 'corewatch#top'
-  get 'corewatch/top', to: 'corewatch#index'
-end
